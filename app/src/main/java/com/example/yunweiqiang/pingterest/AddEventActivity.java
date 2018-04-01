6a2abf675a14cc09bf9fb03ab7eaaff506abba82
Yunwei Qiang <willqiangyw@gmail.com>
2018-03-31 23:38:46 -0400

    add in time picker

--- app/src/main/java/com/example/yunweiqiang/pingterest/AddEventActivity.java
+++ app/src/main/java/com/example/yunweiqiang/pingterest/AddEventActivity.java
@@ -1,10 +1,16 @@
 package com.example.yunweiqiang.pingterest;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -12,7 +18,10 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class AddEventActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class AddEventActivity extends AppCompatActivity
+        implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener{
 
     private EditText mEventKey;
     private EditText mEventTime;
@@ -24,6 +33,8 @@ public class AddEventActivity extends AppCompatActivity {
     private String userEmail;
     private String userKey;
     private DatabaseReference mDatabase;
+
+    public int hour, minute, year, month, day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,5 +73,43 @@ public class AddEventActivity extends AppCompatActivity {
             Toast.makeText(this,"post success", Toast.LENGTH_SHORT).show();
             finish();
         }
+    }
+
+    public void showTimePickerDialog(View v) {
+        final Calendar c = Calendar.getInstance();
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog d = new DatePickerDialog(AddEventActivity.this, this, year, month, day);
+        d.show();
+        // Create a new instance of TimePickerDialog and return it
+
+    }
+
+//    public void pickPlace(View v){
+//        int PLACE_PICKER_REQUEST = 1;
+//        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+//
+//        startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+//    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        this.hour = hourOfDay;
+        this.minute = minute;
+        Toast.makeText(this,year+","+month+","+day+","+this.hour+", "+this.minute, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        this.year = year;
+        this.month = month;
+        this.day = dayOfMonth;
+        TimePickerDialog t = new TimePickerDialog(this, this, hour, minute,
+                DateFormat.is24HourFormat(AddEventActivity.this));
+        t.show();
     }
 }
---
+++ app/src/main/java/com/example/yunweiqiang/pingterest/TimePickerFragment.java
@@ -0,0 +1,36 @@
+package com.example.yunweiqiang.pingterest;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+/**
+ * Created by yunweiqiang on 3/31/18.
+ */
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        // Do something with the time chosen by the user
+        Toast.makeText(getContext(),""+hourOfDay+", "+minute, Toast.LENGTH_LONG).show();
+    }
+
+}
