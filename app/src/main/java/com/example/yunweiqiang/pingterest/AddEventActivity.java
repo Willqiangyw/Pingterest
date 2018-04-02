a34d71ac9515c7994f7d495d26e22d032048df85
Yunwei Qiang <willqiangyw@gmail.com>
2018-04-01 20:09:15 -0400

    refined sign up page

--- app/src/main/java/com/example/yunweiqiang/pingterest/AddEventActivity.java
+++ app/src/main/java/com/example/yunweiqiang/pingterest/AddEventActivity.java
@@ -18,6 +18,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+
+
 import java.util.Calendar;
 
 public class AddEventActivity extends AppCompatActivity
